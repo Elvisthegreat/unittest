ade4e9cf39bd8c1e792e4342a11b1e2e8ae49202
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-02 18:04:04 +0000

    initial commit

---
+++ test_evens.py
@@ -0,0 +1,8 @@
+import unittest
+from evens import even_number_of_evens # importing our function from evens.py
+
+class TestEvens(unittest.TestCase):
+   pass
+
+if __name__ == 'main':
+    unittest.main()
