3df936cd91072e31f6c8de7611496c5e54180d41
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-03 16:45:02 +0000

    Making our function to work

--- test_evens.py
+++ test_evens.py
@@ -2,7 +2,13 @@ import unittest
 from evens import even_number_of_evens # importing our function from evens.py
 
 class TestEvens(unittest.TestCase):
-   pass
+
+   def test_throws_error_if_value_passed_in_is_not_list(self):
+      """
+      raise a TypeError if the value pass into
+      the function is not list
+      """
+      self.assertRaises(TypeError, even_number_of_evens, 4)
 
 if __name__ == 'main':
-    unittest.main()
+   unittest.main()
