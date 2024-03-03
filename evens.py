f2ee695f9065949a615f74af2bfa715d74de5160
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-03 18:30:29 +0000

    Building a function using TDD

--- evens.py
+++ evens.py
@@ -8,7 +8,25 @@ def even_number_of_evens(numbers):
     if the numner of even numbers is even - return True
     """
     
-    return None
+    if isinstance(numbers, list):
+        if numbers == []: # returns False if an empty list is passed in
+            return False
+        else:
+            """
+            count the number of even numbers in the list 
+            and then check if that number itself is even
+            """
+            evens = 0
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+        
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+    else:
+        raise TypeError("A list was not pass into the function")
 
 if __name__ == 'main':
     print(even_number_of_evens(5))
--- test_evens.py
+++ test_evens.py
@@ -9,6 +9,14 @@ class TestEvens(unittest.TestCase):
       the function is not list
       """
       self.assertRaises(TypeError, even_number_of_evens, 4)
+   
+
+   def test_values_in_list(self):
+   
+      self.assertEqual(even_number_of_evens([]), False) # returns False if an empty list is passed in
+      self.assertEqual(even_number_of_evens([2, 4]), True) # return True if two even number are pass into it
+      self.assertEqual(even_number_of_evens([2]), False) # fail if only one even number is passed in
+      self.assertEqual(even_number_of_evens([1, 3, 5]), False) # fail if odd number is pass in has a list
 
 if __name__ == 'main':
    unittest.main()
